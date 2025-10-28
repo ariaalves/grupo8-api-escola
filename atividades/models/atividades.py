5f1c63623d7f789cb4fdc1c2702cadbc8b06d816
Hocrons <holiveira2503@gmail.com>
2025-10-28 15:13:02 -0300

    feat(010): criando entidades do microsserviço atividades

--- atividades/models/atividades.py
+++ atividades/models/atividades.py
@@ -1,1 +1,12 @@
 from . import db
+
+class Atividade(db.Model):
+    __tablename__ = 'atividades'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome_atividade = db.Column(db.String(50),nullable=False)
+    descricao = db.Column(db.String(100))
+    peso_porcento = db.Column(db.Integer,nullable=False)
+    data_entrega = db.Column(db.Date, nullable=False)
+    turma_id = db.Column(db.Integer, db.ForeignKey('turmas.id'), nullable=False)
+    professor_id = db.Column(db.Integer, db.ForeignKey('professor.id'), nullable=False)
