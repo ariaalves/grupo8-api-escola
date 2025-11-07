6c41d443781f1241bbbe829ccd6a87ff6e1cc69c
erikpaulino87 <eriktertuliano9@gmail.com>
2025-11-06 21:51:27 -0300

    feat(011): Criação das entidades reservas

--- reservas/models/reservas.py
+++ reservas/models/reservas.py
@@ -1,1 +1,25 @@
 from . import db
+
+class Reserva(db.Model):
+    __tablename__ = 'reservas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    num_sala = db.Column(db.Integer, nullable=False)
+    lab = db.Column(db.Boolean, default=False)
+    data = db.Column(db.Date, nullable=False)
+    turma_id = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, num_sala, lab, data, turma_id):
+        self.num_sala = num_sala
+        self.lab = lab
+        self.data = data
+        self.turma_id = turma_id
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "num_sala": self.num_sala,
+            "lab": self.lab,
+            "data": self.data.isoformat() if self.data else None,
+            "turma_id": self.turma_id
+        }
